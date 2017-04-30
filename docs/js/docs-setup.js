aa7997b5317bb871603defbedf8d7c8e6079e428
usamahamed <usama.hamed2010@yahoo.com>
2017-04-30 22:16:54 +0200

    Add files via upload

---
+++ docs/js/docs-setup.js
@@ -0,0 +1,34 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "notification-messages",
+      "shortName": "notification-messages",
+      "type": "object",
+      "moduleName": "notification-messages",
+      "shortDescription": "Angular.js module providing notifications using Bootstrap 3 styles and having following features:",
+      "keywords": "$scope ability angular api app appcontroller appearance automatically backend baseurl body bootstrap call called category changed clicking closable closed confirmations controller corresponding created css dafault data default depending details display displayed displays dist don download downloaded feature features fetched files folder forget free front function href html http inject install interface javascript js latest limit link max methods minified module myapp named notification notification-service notification-service-styles notifications number object oldest order overlay pops providing public reached read refer rel responds screen scripts seconds send server service set setoptions side simply spot src styles stylesheet temporary time title usage user var versions"
+    },
+    {
+      "section": "api",
+      "id": "notification-service.service:Notification",
+      "shortName": "Notification",
+      "type": "service",
+      "moduleName": "notification-messages",
+      "shortDescription": "Service to display notifications, talk to server and change default notification settings.",
+      "keywords": "$compile $http $rootscope $timeout amount api app array baseurl call category change content controller corner days default delay display emitter error expires fetched getfromserver header http identifier info int interface javascript limit max method mind ms note notification notification-service notifications notify object ok_cancel_confirm ok_confirm options opts path properties responds screen server service setoptions settings string subscription subscrition talk time title top type usage user usermanagement values warning window"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
